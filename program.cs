2cf4f59639c75650808510520e2021f3fbc13d91
shwetha <skulal386@gmail.com>
2024-09-04 17:32:16 +0530

    pushed Implicit type conversion eg code

---
+++ program.cs
@@ -0,0 +1,23 @@
+using System;
+namespace ITC
+{
+    class SampleTC
+    {
+        public static void main()
+        {
+            int i=57;
+            //implicit type conversion
+            long l=i;
+            //implicit again
+            float f=l;
+            //output
+            console.WriteLine("Int value"+i);
+            console.WriteLine("long value"+l);
+            console.WriteLine("Float value"+f);
+            console.WriteLine("Float value"+f);
+        }
+
+
+    }
+}
+
