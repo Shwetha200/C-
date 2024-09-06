98078ab26afb15d52a058af7cdb2baa9033ccd35
shwetha <skulal386@gmail.com>
2024-09-06 15:58:56 +0530

    explicit type conversion example

---
+++ explicit.cs
@@ -0,0 +1,19 @@
+using System;
+namespace ITC
+{
+    class SampleTC
+    {
+        public static void main()
+        {
+            double d= 97.123;
+            //explicit type conversion
+            int i = (int)d;
+            
+            //output
+            console.WriteLine("Value of i is "+i);
+           
+        }
+
+    }
+}
+
