3bd3756468bb924e40b6c1c41122d98456dda2fd
shwetha <skulal386@gmail.com>
2024-09-20 20:43:16 +0530

    yes

--- programimplicit.cs
+++ programimplicit.cs
@@ -15,7 +15,6 @@ namespace ITC
             console.WriteLine("long value"+l);
             console.WriteLine("Float value"+f);
             console.WriteLine("Float value"+f);
-                        // console.WriteLine("Float value"+f);
 
         }
 
